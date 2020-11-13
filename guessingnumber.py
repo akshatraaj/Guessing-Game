66f3ee8c945155932d4b06c0be3321f7799dd6f5
Akshat Raaj <rvdewsoft@gmail.com>
2020-11-13 19:10:26 +0530

    Add files via upload

---
+++ guessingnumber.py
@@ -0,0 +1,17 @@
+import random
+print("Number guessing game by Akshat Raaj")
+number = random.randint(1, 9)
+chances = 0
+print("Guess a number (between 1 and 9):")
+while chances < 5:
+    guess = int(input("Enter your guess:- "))
+    if guess == number:
+        print("Congratulations 😃 YOU WON!!!")
+        break
+    elif guess < number:
+        print("Your guess was too low- Guess a number higher than", guess)
+    else:
+        print("Your guess was too high- Guess a number lower than", guess)
+    chances += 1
+if not chances < 5:
+    print("YOU LOSE😭 The number is", number)
